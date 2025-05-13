6aed4d18b147cb834f93b6a8b0d5026f98b8515a
razak8 <addorazak8@gmail.com>
2025-05-13 17:13:33 +0000

    added vercel.json file

--- server/server.js
+++ server/server.js
@@ -20,9 +20,3 @@ app.get("/", (req, res) => res.send("API is running..."));
 
 // API routes
 app.use("/api", propertyRouter);
-
-// Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
